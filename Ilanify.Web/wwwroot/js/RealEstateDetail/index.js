0ac5c100047c6ed1565e6aaee1e49b62b19e7f4c
Onur Pıçakcı <98148844+onurpicakci@users.noreply.github.com>
2024-03-18 18:48:49 +0300

    fix change main image

--- Ilanify.Web/wwwroot/js/RealEstateDetail/index.js
+++ Ilanify.Web/wwwroot/js/RealEstateDetail/index.js
@@ -1,38 +1,45 @@
-var currentImageIndex = 0;
-var images = [];
+$(document).ready(function () {
+    var currentImageIndex = 0;
+    var images = $('.detail-thumbnail-image');
+    var thumbnailsWrapper = $('.thumbnails-wrapper');
 
-document.addEventListener('DOMContentLoaded', function () {
-    images = document.querySelectorAll('.detail-thumbnail-image');
-    for (var i = 0; i < images.length; i++) {
-        images[i].addEventListener('click', function (event) {
-            var index = Array.from(event.target.parentNode.children).indexOf(event.target);
-            changeMainImage(index);
-        });
+    images.on('click', function () {
+        changeMainImage($(this).index());
+    });
+
+    $('.left-arrow').on('click', function () {
+        navigateMainImage(-1);
+    });
+
+    $('.right-arrow').on('click', function () {
+        navigateMainImage(1);
+    });
+
+    function changeMainImage(index) {
+        currentImageIndex = index;
+        var mainImage = $('#main-image');
+        var imageUrl = images.eq(currentImageIndex).attr('data-image-url');
+        mainImage.attr('src', imageUrl);
     }
-});
 
-function changeMainImage(index) {
-    currentImageIndex = index;
-    var mainImage = document.getElementById('main-image');
-    var imageUrl = images[index].getAttribute('data-image-url');
-    mainImage.src = imageUrl;
-}
+    function navigateMainImage(step) {
+        var newIndex = (currentImageIndex + step + images.length) % images.length;
+        currentImageIndex = newIndex;
+        changeMainImage(currentImageIndex);
+    }
 
-function navigateMainImage(step) {
-    var newIndex = (currentImageIndex + step + images.length) % images.length;
-    changeMainImage(newIndex);
-}
+    function navigateThumbnail(step) {
+        var scrollAmount = thumbnailsWrapper.scrollLeft() + step * 100; // Adjust as necessary
+        thumbnailsWrapper.animate({scrollLeft: scrollAmount}, 'slow');
+    }
 
-function navigateThumbnail(step) {
-    var thumbnailsWrapper = document.querySelector('.thumbnails-wrapper');
-    thumbnailsWrapper.scrollLeft += step * 100;
-}
+    $('.thumbnail-nav.left-arrow').on('click', function () {
+        navigateThumbnail(-1);
+    });
 
-document.querySelector('.left-arrow').addEventListener('click', function () {
-    navigateMainImage(-1);
-});
-document.querySelector('.right-arrow').addEventListener('click', function () {
-    navigateMainImage(1);
+    $('.thumbnail-nav.right-arrow').on('click', function () {
+        navigateThumbnail(1);
+    });
 });
 
 function showPhoneNumberAndEnableCall(element) {

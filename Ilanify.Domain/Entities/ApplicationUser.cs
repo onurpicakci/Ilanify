2b7528c49d6913e1291dfdc1770c8aadee8f4a5b
Onur Pıçakcı <98148844+onurpicakci@users.noreply.github.com>
2024-03-20 20:03:44 +0300

    Added ProfileImageUrl property to IdentityUser

--- Ilanify.Domain/Entities/ApplicationUser.cs
+++ Ilanify.Domain/Entities/ApplicationUser.cs
@@ -6,5 +6,7 @@ public class ApplicationUser : IdentityUser
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    
+    public string? ProfileImageUrl { get; set; }
     public List<RealEstate> RealEstates { get; set; }
 }

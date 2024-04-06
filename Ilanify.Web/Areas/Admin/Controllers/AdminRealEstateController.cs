dee55f96b797f1fce83e94c014cb2f7989b016f4
Onur Pıçakcı <98148844+onurpicakci@users.noreply.github.com>
2024-04-06 21:50:54 +0300

    Additional edits have been made to the advertisements page

--- Ilanify.Web/Areas/Admin/Controllers/AdminRealEstateController.cs
+++ Ilanify.Web/Areas/Admin/Controllers/AdminRealEstateController.cs
@@ -1,6 +1,6 @@
 using Ilanify.Application.Interfaces;
 using Ilanify.Areas.Admin.Models.ViewModels;
-using Ilanify.Domain.Entities;
+using Ilanify.Domain.Enums;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -12,23 +12,32 @@ namespace Ilanify.Areas.Admin.Controllers
     {
         private readonly IRealEstateService _realEstateService;
         private readonly ICategoryService _categoryService;
+        private readonly IAttributeValueService _attributeValueService;
 
-        public AdminRealEstateController(IRealEstateService realEstateService, ICategoryService categoryService)
+        public AdminRealEstateController(IRealEstateService realEstateService, ICategoryService categoryService,
+            IAttributeValueService attributeValueService)
         {
             _realEstateService = realEstateService;
             _categoryService = categoryService;
+            _attributeValueService = attributeValueService;
         }
-        
+
         public async Task<IActionResult> Index()
         {
             var realEstates = await _realEstateService.GetAllAsync();
             return View(realEstates);
         }
-        
+
         public async Task<IActionResult> Edit(int id)
         {
             var realEstate = await _realEstateService.GetByIdAsync(id);
             @ViewBag.Categories = new SelectList(await _categoryService.GetCategoriesAsync(), "Id", "Name");
+            @ViewBag.Types = Enum.GetValues(typeof(RealEstateType)).Cast<RealEstateType>().Select(v =>
+                new SelectListItem
+                {
+                    Text = v.ToString(),
+                    Value = ((int)v).ToString()
+                }).ToList();
             return View(new AdminRealEstateEditViewModel
             {
                 Id = realEstate.Id,
@@ -36,10 +45,16 @@ namespace Ilanify.Areas.Admin.Controllers
                 Description = realEstate.Description,
                 Price = realEstate.Price,
                 CategoryId = realEstate.CategoryId,
-                SquareMeters = realEstate.SquareMeters
+                SquareMeters = realEstate.SquareMeters,
+                City = realEstate.Location.City,
+                District = realEstate.Location.District,
+                Neighborhood = realEstate.Location.Neighborhood,
+                Type = realEstate.Type,
+                IsActive = realEstate.IsActive
+                //AttributeValues = realEstate.AttributeValues,
             });
         }
-        
+
         [HttpPost]
         public async Task<IActionResult> Edit(AdminRealEstateEditViewModel realEstate)
         {
@@ -51,10 +66,25 @@ namespace Ilanify.Areas.Admin.Controllers
                 entity.Description = realEstate.Description;
                 entity.CategoryId = realEstate.CategoryId;
                 entity.SquareMeters = realEstate.SquareMeters;
+                entity.UpdatedDate = DateTime.Now;
+                entity.Location.City = realEstate.City;
+                entity.Location.District = realEstate.District;
+                entity.Location.Neighborhood = realEstate.Neighborhood;
+                entity.Type = realEstate.Type;
+                entity.IsActive = realEstate.IsActive;
+                //entity.AttributeValues = realEstate.AttributeValues;
                 await _realEstateService.UpdateAsync(entity);
                 return RedirectToAction(nameof(Index));
             }
+
             return View(realEstate);
+        }
+        
+        public async Task<IActionResult> Delete(int id)
+        {
+            var realEstate = await _realEstateService.GetByIdAsync(id);
+            await _realEstateService.DeleteAsync(realEstate);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- Ilanify.Web/Areas/Admin/Models/ViewModels/AdminRealEstateEditViewModel.cs
+++ Ilanify.Web/Areas/Admin/Models/ViewModels/AdminRealEstateEditViewModel.cs
@@ -1,19 +1,21 @@
 using Ilanify.Domain.Entities;
+using Ilanify.Domain.Enums;
 
 namespace Ilanify.Areas.Admin.Models.ViewModels;
 
 public class AdminRealEstateEditViewModel
 {
     public int Id { get; set; }
-    
     public string Title { get; set; }
-    
     public string Description { get; set; }
     
     public Category? Category { get; set; }
-    
     public decimal Price { get; set; }
     public int CategoryId { get; set; }
-    
     public int? SquareMeters { get; set; }
+    public string City { get; set; }
+    public string District { get; set; }
+    public string Neighborhood { get; set; }
+    public bool IsActive { get; set; }
+    public RealEstateType Type { get; set; }
 }

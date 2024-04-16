fad320a9f0a907407ca9f7c67baf436927fdecb4
Onur Pıçakcı <98148844+onurpicakci@users.noreply.github.com>
2024-04-16 14:01:18 +0300

    css correction made

--- Ilanify.Domain/Entities/ApplicationUser.cs
+++ Ilanify.Domain/Entities/ApplicationUser.cs
@@ -6,7 +6,6 @@ public class ApplicationUser : IdentityUser
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    
     public string? ProfileImageUrl { get; set; }
     public List<RealEstate> RealEstates { get; set; }
 }

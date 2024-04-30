6a7b4ec9efc6721392c13d1db84d164dc9c1a68a
Onur Pıçakcı <98148844+onurpicakci@users.noreply.github.com>
2024-04-30 23:16:18 +0300

    final edits to the admin panel have been made

--- Ilanify.Web/Areas/Admin/Controllers/AdminHomeController.cs
+++ Ilanify.Web/Areas/Admin/Controllers/AdminHomeController.cs
@@ -1,6 +1,9 @@
 using Ilanify.Application.Interfaces;
+using Ilanify.Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ilanify.Areas.Admin.Controllers
 {
@@ -10,15 +13,25 @@ namespace Ilanify.Areas.Admin.Controllers
     public class AdminHomeController : Controller
     {
         private readonly IRealEstateService _realEstateService;
+        private readonly ICategoryService _categoryService;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public AdminHomeController(IRealEstateService realEstateService)
+        public AdminHomeController(IRealEstateService realEstateService, ICategoryService categoryService, UserManager<ApplicationUser> userManager)
         {
             _realEstateService = realEstateService;
+            _categoryService = categoryService;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> Index()
         {
             var realEstatesCount = await _realEstateService.GetRealEstatesCount();
+            
+            var totalCategories = await _categoryService.GetAllAsync();
+            ViewBag.Categories = totalCategories.Count();
+
+            var totalRegisteredUsers = await _userManager.Users.CountAsync();
+            ViewBag.RegisteredUsers = totalRegisteredUsers;
             return View(realEstatesCount);
         }
     }
